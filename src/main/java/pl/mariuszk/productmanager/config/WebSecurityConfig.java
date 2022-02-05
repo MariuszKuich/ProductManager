e69810578f3d5bd3e5a846d5be238edf0f0588b8
MariuszKuich <mariuszkuich@outlook.com>
2022-02-05 19:22:14 +0100

    add spring security and login page

---
+++ src/main/java/pl/mariuszk/productmanager/config/WebSecurityConfig.java
@@ -0,0 +1,49 @@
+package pl.mariuszk.productmanager.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final EncoderConfig encoderConfig;
+
+    @Override
+    public void configure(WebSecurity webSecurity) {
+        webSecurity
+                .ignoring()
+                .antMatchers("/resources/**", "/static/**","/webjars/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeRequests()
+                    .anyRequest()
+                    .authenticated()
+                .and()
+                    .formLogin()
+                    .loginPage("/login")
+                    .permitAll()
+                .and()
+                    .logout()
+                    .permitAll();
+    }
+
+    @Override
+    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("markui000").password(encoderConfig.passwordEncoder().encode("a")).roles("USER")
+                .and()
+                .withUser("andjur001").password(encoderConfig.passwordEncoder().encode("3ypDTV")).roles("USER")
+                .and()
+                .withUser("kampol002").password(encoderConfig.passwordEncoder().encode("ML2KxW")).roles("USER");
+    }
+}
